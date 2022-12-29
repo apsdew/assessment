21f03184597fc4c2101becd731c8bccc82094a9f
Apisit Pankaew <aps.dew@gmail.com>
2022-12-29 23:56:27 +0700

    add Handler create, get, getall

---
+++ expenses/get.go
@@ -0,0 +1,30 @@
+package expenses
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/lib/pq"
+)
+
+func GetExpensesHandler(c echo.Context) error {
+	id := c.Param("id")
+	stmt, err := db.Prepare("SELECT * FROM expenses WHERE id = $1")
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: "can't prepare query expenses statment:" + err.Error()})
+	}
+
+	row := stmt.QueryRow(id)
+	e := Expenses{}
+	err = row.Scan(&e.ID, &e.Title, &e.Amount, &e.Note, pq.Array(&e.Tags))
+	switch err {
+	case sql.ErrNoRows:
+		return c.JSON(http.StatusNotFound, Err{Message: "expenses not found"})
+	case nil:
+		return c.JSON(http.StatusOK, e)
+	default:
+		return c.JSON(http.StatusInternalServerError, Err{Message: "can't scan expenses:" + err.Error()})
+	}
+}
